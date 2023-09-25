a892c82b3a8a3b615a4bebf8813abd011a6b583b
Iker González <84924263+iker-gonzalez@users.noreply.github.com>
2023-09-25 07:56:26 +0000

    mariadb init successfull

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 # Start mysql service
-/etc/init.d/mysql start
+/etc/init.d/mysql start 
 
-# Wait for it to start up
-sleep 5
+# Wait for it to fully start
+sleep 10
 
-# Create database 
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $MYSQL_DATABASE;"
+# Debugging: Print the SQL queries being executed
+set -x
 
-# Create user 
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+# Create database without double quotes for variable
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE $MYSQL_DATABASE"
+
+# Create user with proper SQL syntax 
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+
+# Grant privileges 
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO '$MYSQL_USER'@'%';"  
 
 # Flush privileges
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+
+# Debugging: Disable debugging output
+set +x

0854731e2044a8be743bd0d53c8fb4bc44d9501f
Iker González <84924263+iker-gonzalez@users.noreply.github.com>
2023-10-01 09:39:35 +0000

    sql db init sh expanded

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -4,28 +4,33 @@
 /etc/init.d/mysql start
 
 # Wait for it to fully start
-sleep 10
+# sleep 10
 
-# Debugging: Print the SQL queries being executed
-set -x
+# Check if the database exists
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+    echo "Database already exists"
+else
+    # Debugging: Print the SQL queries being executed
+    set -x
 
-# Create database
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE $MYSQL_DATABASE"
+    # Create database
+    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE $MYSQL_DATABASE"
 
-# Create a non-standard administrator user
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER '$MYSQL_ADMIN_USER'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';"
+    # Create a non-standard administrator user
+    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER '$MYSQL_ADMIN_USER'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';"
 
-# Grant administrative privileges
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ADMIN_USER'@'%';"
+    # Grant administrative privileges
+    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ADMIN_USER'@'%';"
 
-# Create a regular user
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+    # Create a regular user
+    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 
-# Grant privileges to the regular user
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO '$MYSQL_USER'@'%';"
+    # Grant privileges to the regular user
+    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO '$MYSQL_USER'@'%';"
 
-# Flush privileges
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+    # Flush privileges
+    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
-# Debugging: Disable debugging output
-set +x
+    # Debugging: Disable debugging output
+    set +x
+fi

9f7cd8e745cf47663e3035b9715179b9e2e21677
Iker González <84924263+iker-gonzalez@users.noreply.github.com>
2023-10-01 08:00:20 +0000

    extra user (admin) added to database

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start mysql service
-/etc/init.d/mysql start 
+/etc/init.d/mysql start
 
 # Wait for it to fully start
 sleep 10
@@ -9,14 +9,20 @@ sleep 10
 # Debugging: Print the SQL queries being executed
 set -x
 
-# Create database without double quotes for variable
+# Create database
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE $MYSQL_DATABASE"
 
-# Create user with proper SQL syntax 
+# Create a non-standard administrator user
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER '$MYSQL_ADMIN_USER'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';"
+
+# Grant administrative privileges
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ADMIN_USER'@'%';"
+
+# Create a regular user
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 
-# Grant privileges 
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO '$MYSQL_USER'@'%';"  
+# Grant privileges to the regular user
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO '$MYSQL_USER'@'%';"
 
 # Flush privileges
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"

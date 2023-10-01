e3d93dfcbbccbea8ead744276509fbd3c24b9049
Iker González <84924263+iker-gonzalez@users.noreply.github.com>
2023-10-01 11:24:53 +0000

    volumes binded to host machine

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -14,23 +14,23 @@ else
     set -x
 
     # Create database
-    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE $MYSQL_DATABASE"
+    mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "CREATE DATABASE $MYSQL_DATABASE"
 
     # Create a non-standard administrator user
-    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER '$MYSQL_ADMIN_USER'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';"
+    mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "CREATE USER '$MYSQL_ADMIN_USER'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';"
 
     # Grant administrative privileges
-    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ADMIN_USER'@'%';"
+    mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ADMIN_USER'@'%';"
 
     # Create a regular user
-    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+    mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "CREATE USER '$MYSQL_STANDARD_USER'@'%' IDENTIFIED BY '$MYSQL_STANDARD_PASSWORD';"
 
     # Grant privileges to the regular user
-    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO '$MYSQL_USER'@'%';"
+    mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "GRANT ALL ON *.* TO '$MYSQL_STANDARD_USER'@'%';"
 
     # Flush privileges
-    mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+    mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "FLUSH PRIVILEGES;"
 
     # Debugging: Disable debugging output
-    set +x
+    #set +x
 fi

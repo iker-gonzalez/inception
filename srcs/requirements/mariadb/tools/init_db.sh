8b9053d714b1108512bc8c4b5d1b1f3870317ad4
Iker González <84924263+iker-gonzalez@users.noreply.github.com>
2023-10-12 15:23:35 +0000

    add conditional creations to wordpress and mariadb

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,35 +1,46 @@
 #!/bin/bash
 
-# Start mysql service
+mysql_install_db
+
 /etc/init.d/mysql start
 
 # Wait for it to fully start
 sleep 5
 
-# Debugging: Print the SQL queries being executed
-set -x
+#Check if the database exists
 
-# Create database
-mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "CREATE DATABASE $MYSQL_DATABASE"
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
+then 
 
-# Create a non-standard administrator user
-mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "CREATE USER '$MYSQL_ADMIN_USER'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';"
+	echo "Database already exists"
+else
 
-# Grant administrative privileges
-mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ADMIN_USER'@'%';"
+    # Debugging: Print the SQL queries being executed
+    set -x
 
-# Create a regular user
-mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "CREATE USER '$MYSQL_STANDARD_USER'@'%' IDENTIFIED BY '$MYSQL_STANDARD_PASSWORD';"
+    # Create database
+    mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "CREATE DATABASE $MYSQL_DATABASE"
 
-# Grant privileges to the regular user
-mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_STANDARD_USER'@'%';"
+    # Create a non-standard administrator user
+    mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "CREATE USER '$MYSQL_ADMIN_USER'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';"
 
-# Flush privileges
-mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "FLUSH PRIVILEGES;"
+    # Grant administrative privileges
+    mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ADMIN_USER'@'%';"
 
-# Debugging: Disable debugging output
-set +x
+    # Create a regular user
+    mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "CREATE USER '$MYSQL_STANDARD_USER'@'%' IDENTIFIED BY '$MYSQL_STANDARD_PASSWORD';"
 
-#/etc/init.d/mysql stop
+    # Grant privileges to the regular user
+    mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_STANDARD_USER'@'%';"
+
+    # Flush privileges
+    mysql -u root -p"$MYSQL_ADMIN_PASSWORD" -e "FLUSH PRIVILEGES;"
+
+    # Debugging: Disable debugging output
+    #set +x
+
+fi
+
+/etc/init.d/mysql stop
 
 exec "$@"
--- srcs/requirements/wordpress/tools/set_wordpress.sh
+++ srcs/requirements/wordpress/tools/set_wordpress.sh
@@ -1,27 +1,33 @@
 #!/bin/sh
 
+if [ -f ./wordpress/wp-config.php ]
+then
+	echo "wordpress already downloaded"
+else
 
-#Download wordpress
-wget https://wordpress.org/latest.tar.gz
-tar -xzvf latest.tar.gz
-rm -rf latest.tar.gz
+    #Download wordpress
+    wget https://wordpress.org/latest.tar.gz
+    tar -xzvf latest.tar.gz
+    rm -rf latest.tar.gz
 
-# Move the new file to the destination, overwriting any existing file
-mv -f ./www.conf /etc/php/7.3/fpm/pool.d/www.conf
+    # Move the new file to the destination, overwriting any existing file
+    mv -f ./www.conf /etc/php/7.3/fpm/pool.d/www.conf
 
-# Import environment variables into wp-config.php
-#cd /var/www/html/wordpress
-#sed -i "s/username_here/$MYSQL_STANDARD_USER/g" wp-config-sample.php
-#sed -i "s/password_here/$MYSQL_STANDARD_PASSWORD/g" wp-config-sample.php
-#sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-#sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+    # Import environment variables into wp-config.php
+    cd /var/www/html/wordpress
+    sed -i "s/username_here/$MYSQL_STANDARD_USER/g" wp-config-sample.php
+    sed -i "s/password_here/$MYSQL_STANDARD_PASSWORD/g" wp-config-sample.php
+    sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+    sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 
-# Add WordPress debugging options
-echo "define('WP_DEBUG', true);" >> wp-config-sample.php
-echo "define('WP_DEBUG_LOG', true);" >> wp-config-sample.php
-echo "define('WP_DEBUG_DISPLAY', false);" >> wp-config-sample.php
+    # Add WordPress debugging options
+    #echo "define('WP_DEBUG', true);" >> wp-config-sample.php
+    #echo "define('WP_DEBUG_LOG', true);" >> wp-config-sample.php
+    #echo "define('WP_DEBUG_DISPLAY', false);" >> wp-config-sample.php
 
-# Rename wp-config-sample.php to wp-config.php
-#mv wp-config-sample.php wp-config.php
+    # Rename wp-config-sample.php to wp-config.php
+    mv wp-config-sample.php wp-config.php
+
+fi
 
 exec "$@"

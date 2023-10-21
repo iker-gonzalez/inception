9632c1ae841a95de002a62d5808aec7d29e97faf
Iker González <84924263+iker-gonzalez@users.noreply.github.com>
2023-10-21 11:44:43 +0000

    init DB script refactored

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -4,50 +4,29 @@
 set -x
 
 # Set the password for the root user
-mysqladmin -u root password $MYSQL_ROOT_PASSWORD
+mysqladmin -u root password "$MYSQL_ROOT_PASSWORD"
 
-# Force MySQL root user to use a password
-sed -i '/^\[mysqld\]$/a skip-grant-tables' /etc/mysql/mysql.conf.d/mysqld.cnf
+# Modify the MySQL configuration file to enable password authentication
+sed -i '/^\[mysqld\]$/a skip-grant-tables=0' /etc/mysql/mariadb.cnf
 
-mysql_install_db
-
+# Start MySQL server
 /etc/init.d/mysql start
 
 # Wait for it to fully start
 sleep 5
 
-# Check if the database exists
+# Create database, users, and privileges
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$MYSQL_ADMIN_USER'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ADMIN_USER'@'%';"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$MYSQL_STANDARD_USER'@'%' IDENTIFIED BY '$MYSQL_STANDARD_PASSWORD';"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_STANDARD_USER'@'%';"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES"
 
-# if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
-# then 
+# Debugging: Disable debugging output
+set +x
 
-#     echo "Database already exists"
-# else
-
-
-    # Create database
-    mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $MYSQL_DATABASE"
-
-    # Create a non-standard administrator user
-    mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER '$MYSQL_ADMIN_USER'@'%' IDENTIFIED BY '$MYSQL_ADMIN_PASSWORD';"
-
-    # Grant administrative privileges
-    mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ADMIN_USER'@'%';"
-
-    # Create a regular user
-    mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER '$MYSQL_STANDARD_USER'@'%' IDENTIFIED BY '$MYSQL_STANDARD_PASSWORD';"
-
-    # Grant privileges to the regular user
-    mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_STANDARD_USER'@'%';"
-
-    # Flush privileges
-    mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-
-    # Debugging: Disable debugging output
-    set +x
-
-# fi
-
+# Properly stop MySQL server
 /etc/init.d/mysql stop
 
 exec "$@"

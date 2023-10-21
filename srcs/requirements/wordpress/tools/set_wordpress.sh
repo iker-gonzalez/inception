0909878f8b7b163c7fea16ea1fafa7b0e1f53026
Iker González <84924263+iker-gonzalez@users.noreply.github.com>
2023-10-21 15:54:11 +0000

    url domain name updated

--- srcs/requirements/wordpress/tools/set_wordpress.sh
+++ srcs/requirements/wordpress/tools/set_wordpress.sh
@@ -35,7 +35,7 @@ else
     wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_STANDARD_USER --dbpass=$MYSQL_STANDARD_PASSWORD --dbhost=$MYSQL_HOSTNAME --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
 
     # Install WordPress CLI and create users
-    wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+    wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
     wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --user_pass=$WP_USER_PASSWORD --allow-root
 
 
